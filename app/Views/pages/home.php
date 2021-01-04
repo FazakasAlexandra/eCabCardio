09ed1b02c59aa88cd6dbce7559f66befda45b432
Alexandra Fazakas <alexandra.fazakas91@gmail.com>
2021-01-04 20:56:11 +0200

    update

--- app/Views/pages/home.php
+++ app/Views/pages/home.php
@@ -1,4 +1,4 @@
 	<div class="welcome-message">
-		<h3 class="welcome-message text">WELCOME TO eCAB</h3>
+		<h3 class="welcome-message text">WELCOME TO ECABCARDIO</h3>
 		<img src="assets/doctor.jpg" alt="" class="welcome-message image">
 	</div>
