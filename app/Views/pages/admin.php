f84471e4b3951552db3ea8223122e01edb96cbd7
Hirtop Laura <lhirtop@gmail.com>
2021-01-16 23:53:58 +0200

    added examinations modification page with problems-shouldn't affect you, just ignore :))

---
+++ app/Views/pages/admin.php
@@ -0,0 +1,14 @@
+<div class="container">
+    <div class="row" style="margin-top:50px;">
+        <div class="col">
+            <div class="btn-group btn-group-lg" role="group">
+                <button id="btnGroupDrop1" type="button" class="btn btn-primary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">Modify Data</button>
+                <ul class="dropdown-menu" aria-labelledby="btnGroupDrop1">
+                    <li><a class="dropdown-item" href="http://localhost/ecabcardio/public/admin/examinations">Examinations</a></li>
+                    <li><a class="dropdown-item" href="#">Analisys</a></li>
+                    <li><a class="dropdown-item" href="#">Patients</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
